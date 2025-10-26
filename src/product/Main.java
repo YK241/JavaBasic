a29e89886d375d4949bc8dcffe9f5482f9165ab8
YK <>
2025-10-26 19:48:38 +0900

    kadai5

--- src/product/Main.java
+++ src/product/Main.java
@@ -1,12 +1,14 @@
 package product;
 
+import java.util.List;
+
 public class Main {
 
 	public static void showProduct(Product p) {
 		if (p == null) {
 			System.out.println("商品が存在しません");
 		} else {
-			System.out.println(
+			System.out.print(
 					"Product: id=" + p.getId() +
 							", name=" + p.getName() +
 							", price=" + p.getPrice() +
@@ -20,12 +22,25 @@ public class Main {
 					"Product: id=" + p.getId() +
 							", name=" + p.getName() +
 							", price=" + p.getPrice() +
-							", stock=" + p.getStock() );
+							", stock=" + p.getStock());
+		}
+	}
+
+	public static void showDiscountProduct(DiscountProduct dp) {
+		if (dp == null) {
+			System.out.println("商品が存在しません");
+		} else {
+			System.out.print(
+					"Product: id=" + dp.getId() +
+							", name=" + dp.getName() +
+							", price=" + dp.getPrice() +
+							", stock=" + dp.getStock() +
+							", 割引率=" + dp.getDiscountRate());
 		}
 	}
 
 	public static void main(String[] args) {
-		System.out.println("---商品を5つ追加して全て表示---");
+		//System.out.println("---商品を5つ追加して全て表示---");
 		ProductManager manager = new ProductManager();
 
 		Product p1 = new Product(1, "冷蔵庫", 50000, 10);
@@ -40,18 +55,42 @@ public class Main {
 		manager.addProduct(p4);
 		manager.addProduct(p5);
 
-		showAllProduct(manager);
+		/*showAllProduct(manager);
 		System.out.println();
-
+		
 		System.out.println("---商品を一つ削除して全て表示---");
-
+		
 		manager.removeProduct(1);
-
+		
 		showAllProduct(manager);
 		System.out.println();
-
+		
 		System.out.println("---商品名「米」の情報を表示する---");
 		showProduct(manager.getProductByName("米"));
-	}
+		*/
 
+		DiscountProduct dp = new DiscountProduct(
+				p2.getId(),
+				p2.getName(),
+				p2.getPrice(),
+				p2.getStock(),
+				0.3);
+
+		int dicountedPrice = dp.calculateDiscountedPrice();
+
+		System.out.println("---商品名「ソファ」の情報と割引率30%の情報を表示する---");
+		showDiscountProduct(dp);
+		System.out.println(",割引後価格=" + dicountedPrice);
+
+		
+		System.out.println("---商品名「Tシャツ」を検索して表示する---");
+		List<Product> results = manager.search("Tシャツ");
+		if (results.isEmpty()) {
+			System.out.println("該当する商品がありません。");
+		} else {
+			for (Product p : results) {
+				showProduct(p);
+			}
+		}
+	}
 }
