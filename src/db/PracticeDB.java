a8f33eb5a1b31e00b83dec678f2b95a1f3ca7dab
YK <>
2025-11-02 17:33:55 +0900

    kadai7

--- src/db/PracticeDB.java
+++ src/db/PracticeDB.java
@@ -6,41 +6,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class ProductDB {
+public class PracticeDB {
 	public static void main(String[] args) {
 		String url = "jdbc:mysql://localhost:3306/product_management?serverTimezone=UTC";
 		String user = "root";
 		String pass = "pass"; // mysqlのpassの為、仮名
 
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+		String sql = "SELECT * FROM products";
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
-			conn = DriverManager.getConnection(url, user, pass);
-			System.out.println("DB接続成功");
-			System.out.println(	"--productsテーブルのすべての商品情報を表示--");
+			try (
+					Connection conn = DriverManager.getConnection(url, user, pass);
+					Statement stmt = conn.createStatement();
+					ResultSet rs = stmt.executeQuery(sql);) {
+				System.out.println("DB接続成功");
+				System.out.println("--productsテーブルのすべての商品情報を表示--");
 
-			stmt = conn.createStatement();
+				while (rs.next()) {
+					int id = rs.getInt("id");
+					String name = rs.getString("name");
+					int price = rs.getInt("price");
+					int stock = rs.getInt("stock");
+					int categoryId = rs.getInt("category_id");
 
-			String sql = "SELECT * FROM products";
-			rs = stmt.executeQuery(sql);
-
-			while (rs.next()) {
-				int id = rs.getInt("id");
-				String name = rs.getString("name");
-				int price = rs.getInt("price");
-				int stock = rs.getInt("stock");
-				int categoryId = rs.getInt("category_id");
-
-				System.out.println(
-								"id:" + id + "\n" +
-								"name:" + name + "\n" +
-								"price:" + price + "\n" +
-								"stock:" + stock + "\n" +
-								"category_id:" + categoryId + "\n");
+					System.out.println(
+							"id:" + id + "\n" +
+									"name:" + name + "\n" +
+									"price:" + price + "\n" +
+									"stock:" + stock + "\n" +
+									"category_id:" + categoryId + "\n");
+				}
 			}
 		} catch (ClassNotFoundException e) {
 			System.out.println("DB接続失敗");
@@ -50,17 +47,6 @@ public class ProductDB {
 			System.out.println("DB接続失敗");
 			System.out.println("DB接続エラー");
 			e.printStackTrace();
-		} finally {
-			try {
-				if (rs != null)
-					rs.close();
-				if (stmt != null)
-					stmt.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				System.out.println("リソースクローズエラー: " + e.getMessage());
-			}
 		}
 	}
 }
